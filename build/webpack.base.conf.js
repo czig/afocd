548afffcbaa44de47c9a28060750a71cf4efebea
Caleb <caleb.ziegler@gmail.com>
2018-11-20 16:05:05 -0600

    Fixes build process for apache web server.

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -68,7 +68,9 @@ module.exports = {
         loader: 'url-loader',
         options: {
           limit: 10000,
-          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+          name: utils.assetsPath('fonts/[name].[hash:7].[ext]'),
+          publicPath: '../../'
+
         }
       },
       {
